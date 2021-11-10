c69dc3f1e8e16afbc03d291d4f8e3a22c97656ac
enomilan <andrewmusta@gmail.com>
2021-11-10 10:35:26 -0600

    dom

---
+++ frontend-dom/src/index.js
@@ -0,0 +1,12 @@
+const root = document.getElementById('root');
+const div1 = document.createElement('div');
+div1.innerText= 'div1'
+
+root.appendChild(div1)
+
+const render = () => {
+
+        const App= document.createElement('div')
+        App.innerHTML = ''
+
+}
